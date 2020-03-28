ae8355086428c76a7233ec41a48f650143491756
helibertoarias <HelibertoArias@gmail.com>
2020-03-28 17:50:43 -0500

    Feature: Add new components and enable a basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,32 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { FormsModule } from "@angular/forms";
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from "@angular/common/http";
+import { ListComponent } from './list/list.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
 import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      BrowserAnimationsModule
+      BrowserAnimationsModule,
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
