0a83839b22380377e59245e2b3e116630f7944a6
helibertoarias <HelibertoArias@gmail.com>
2020-03-28 17:06:26 -0500

    Feature: Add Bootswatch and NgxBoostrap components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-
-
+import { AuthService } from './_services/auth.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { FormsModule } from "@angular/forms";
+import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from "@angular/common/http";
 import { NavComponent } from './nav/nav.component';
-
-import { FormsModule } from "@angular/forms";
-import { AuthService } from './_services/auth.service';
-import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
 
 @NgModule({
    declarations: [
@@ -23,7 +21,9 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot(),
+      BrowserAnimationsModule
    ],
    providers: [
       AuthService,
